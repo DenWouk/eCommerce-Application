cd63f37625f31501e0463c423fd26af68fdf7476
DenWouk <info.d1@protonmail.com>
2023-08-15 03:12:03 +0300

    feat: create basic header

---
+++ src/components/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import Footer from './footer';
+import Header from './header';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}

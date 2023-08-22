d048a66a33310ee19efb5aa3ff23385d6e2d5d5b
Kirill <zakalupali@gmail.com>
2023-08-22 21:32:00 +0300

    fix: fix InputDate

--- src/components/InputDate.tsx
+++ src/components/InputDate.tsx
@@ -10,17 +10,9 @@ export default function InputDate({ control }: ITextParams) {
     return copyDate;
   }
   const minBirthdate = minYear(new Date(), 13);
-
-  const validateDate = {
-    required: 'Birth Date is required',
-    validate: (value: any) => {
-      const dob = new Date(value);
-      return dob >= minBirthdate || 'You must be at least 13 years old';
-    },
-  };
-  // const error = get(errors, name);
   const maxDate = new Date();
   maxDate.setFullYear(maxDate.getFullYear() - 13);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <Controller
@@ -32,15 +24,23 @@ export default function InputDate({ control }: ITextParams) {
             min: (date) => (date as Date) <= minBirthdate || 'Please, enter a valid date',
           },
         }}
-        render={({ field: { value, onChange } }) => (
+        render={({ field: { ref, onBlur, name, onChange, ...field }, fieldState }) => (
           <DatePicker
-            value={value}
+            {...field}
+            inputRef={ref}
             onChange={onChange}
             label="Date of Birth"
             className="dark:bg-white"
             maxDate={maxDate}
-            // error={fieldState.invalid} // Set error state based on field validation
-            // helperText={fieldState.error?.message || ''}
+            slotProps={{
+              textField: {
+                required: true,
+                onBlur,
+                name,
+                error: !!fieldState?.error,
+                helperText: fieldState?.error?.message,
+              },
+            }}
           />
         )}
       />

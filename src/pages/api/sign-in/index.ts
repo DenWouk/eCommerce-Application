386861af7aeff3b86b295bb04c762834a8cc3cbb
Kirill <zakalupali@gmail.com>
2023-08-25 13:58:21 +0300

    refactor: removed 'createValueCookieToken' from helpers and its usage in 'sign-in' API due to a change in authentication method.

--- src/pages/api/sign-in/index.ts
+++ src/pages/api/sign-in/index.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { UserAuthOptions } from '@commercetools/sdk-client-v2';
-import tokenCache from '@/src/helpers/commercetools/tokenCache';
-import TOKEN_KEY from '@/src/constats';
 import CustomerModel from '@/src/models/cutomer';
-import createValueCookieToken from '@/src/helpers/commercetools/cookeis';
 
 type NextApiRequestWithBody = Omit<NextApiRequest, 'body'> & {
   body: UserAuthOptions;
@@ -15,13 +12,7 @@ export default async function handler(req: NextApiRequestWithBody, res: NextApiR
     new CustomerModel()
       .signIn({ email: body.username, password: body.password })
       .then((me) => {
-        const { token, expirationTime } = tokenCache.get();
-        const cookieValue = createValueCookieToken({ token, expirationTime });
-        res
-          .setHeader(TOKEN_KEY, tokenCache.get().token)
-          .setHeader('Set-Cookie', cookieValue)
-          .status(200)
-          .json(me);
+        res.status(200).json(me);
       })
       .catch((e) => {
         const code = e?.body?.errors?.[0]?.code;

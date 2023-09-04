0f9e2e5ddb0ea0ade28fcc105847d3eb48df3886
Kirill <zakalupali@gmail.com>
2023-09-04 14:03:11 +0300

    fix: fix client building process and user authentication flow; fix path for profile page in middleware.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ import { AuthOptions } from 'next-auth/core/types';
 import { getToken, GetTokenParams } from 'next-auth/jwt';
 import { getCookieParser } from 'next/dist/server/api-utils';
 import NamesClients from '@/src/helpers/commercetools/consts';
-import builderApiRoot, { TypeBuilderApiRoot } from '@/src/helpers/commercetools/builderApiRoot';
+import builderApiRoot from '@/src/helpers/commercetools/builderApiRoot';
 import { refreshAccessToken, tokenCache } from '@/src/helpers/commercetools/token';
 
 const builder = builderApiRoot;
@@ -59,47 +59,41 @@ export const authOptions: AuthOptions = {
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
-        tokenCache.clear();
         const cookies = req?.headers && getCookieParser(req.headers)();
-        let typeBuilder: keyof Pick<TypeBuilderApiRoot, 'createForUser' | 'createForAnonymous'> =
-          'createForUser';
         let isAnonymousSession: boolean = false;
         if (cookies) {
           const jwt = await getToken({ req: { cookies, headers: req.headers } } as GetTokenParams);
           const { token, type } = jwt || {};
           isAnonymousSession = type === NamesClients.ANONYMOUS && !!token;
-          if (isAnonymousSession) {
-            typeBuilder = 'createForAnonymous' as const;
-          }
         }
         if (!credentials) {
           throw new Error('Username and password were not provided');
         }
 
-        const client = builder[typeBuilder](credentials);
         let customer: Customer | undefined;
         try {
-          customer = (
-            await client
-              .me()
-              .login()
-              .post({
-                body: {
-                  email: credentials.username,
-                  password: credentials.password,
-                },
-              })
-              .execute()
-          ).body.customer;
-
           if (isAnonymousSession) {
             tokenCache.clear();
-            customer = (await builder.createForUser(credentials).me().get().execute()).body;
+            customer = (
+              await builder
+                .createForAnonymous()
+                .me()
+                .login()
+                .post({
+                  body: {
+                    email: credentials.username,
+                    password: credentials.password,
+                  },
+                })
+                .execute()
+            ).body.customer;
           }
+          tokenCache.clear();
+          customer = (await builder.createForUser(credentials).me().get().execute()).body;
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (e: any) {
           const code = e?.body?.errors?.[0]?.code;
-          if (code === 'invalid_customer_account_credentials') {
+          if (code === 'invalid_customer_account_credentials' || code === 'InvalidCredentials') {
             throw new Error(e.message);
           } else {
             throw new Error('Oops, something went wrong, try again later');
@@ -119,22 +113,27 @@ export const authOptions: AuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, trigger, session }) {
+      const copyToken = { ...token };
       if (user) {
         const { token: tokenStore, id, type } = user;
-        return { ...token, id, type, token: tokenStore };
+        return { ...copyToken, id, type, token: tokenStore };
       }
 
-      if (Date.now() < token.token.expirationTime) {
-        return token;
+      if (trigger === 'update') {
+        copyToken.name = session.name;
       }
 
-      return refreshAccessToken(token);
+      if (Date.now() < copyToken.token.expirationTime) {
+        return copyToken;
+      }
+
+      return refreshAccessToken(copyToken);
     },
 
     async session({ session, token }) {
-      const { id, type } = token;
-      return { ...session, type, id };
+      const { id, type, name } = token;
+      return { ...session, type, id, user: { ...session.user, name } };
     },
   },
   session: {

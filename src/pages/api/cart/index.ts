5a92a70810c06dc78fd0974f84a9fa10d92187ea
Tanya Lisna <tplovic@gmail.com>
2023-09-13 12:06:38 +0300

    add: cart api in pages

--- src/pages/api/cart/index.ts
+++ src/pages/api/cart/index.ts
@@ -1,23 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import productModel from '@/src/helpers/commercetools/product';
 import { handlerAuthToken } from '@/src/helpers/next/withAuthToken';
+import { UpdateCartWithTypeAction } from '@/src/types/commercetools';
+import cartModel from '@/src/helpers/commercetools/cart';
 
-export default handlerAuthToken(
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'GET') {
-      await productModel
-        .getProducts(req, req.query)
-        .then((products) => {
-          res.status(200).json(products);
-        })
-        .catch((e) => {
-          const code = e?.body?.errors?.[0]?.code;
-          if (code === 'InvalidCredentials' || code === 'invalid_customer_account_credentials') {
-            res.status(400).json({ message: e.message });
-          } else {
-            res.status(400).json({ message: 'Oops, something went wrong, try again later' });
-          }
-        });
-    }
+export default handlerAuthToken(async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    await cartModel
+      .getCart(req)
+      .then((cartsResponse) => {
+        res.status(200).json(cartsResponse);
+      })
+      .catch(() => {
+        res.status(400).json({ message: 'Oops, something went wrong, try again later' });
+      });
   }
-);
+
+  if (req.method === 'POST') {
+    const body = req.body as UpdateCartWithTypeAction;
+    await cartModel
+      .updateCart(req, body)
+      .then((cartsResponse) => {
+        res.status(200).json(cartsResponse);
+      })
+      .catch(() => {
+        res.status(400).json({ message: 'Oops, something went wrong, try again later' });
+      });
+  }
+});

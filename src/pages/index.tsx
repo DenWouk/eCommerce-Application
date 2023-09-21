44096aa06c1af3ad2fcf4bcae29cc85f2aa71356
Den <info.d1@protonmail.com>
2023-09-21 04:47:05 +0300

    refactor: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,16 @@
-import { Box } from '@mui/material';
 import NamesClients from '@/src/helpers/commercetools/consts';
 import { ssrWithAuthToken } from '@/src/helpers/next/withAuthToken';
 
 export default function Home() {
   return (
     <div className="main-page-background">
-        <div className="main-page-title">Welcome to our autohouse!</div>
+      <div className="main-page-title">Welcome to our autohouse!</div>
 
-      <div className="main-page-promocodes">Catch Promo Codes
-      <div className="promocode-1">LuckyCustomer10</div>
-      <div className="promocode-2">bmw-10</div>
+      <div className="main-page-promocodes">
+        Catch Promo Codes
+        <div className="promocode-1">LuckyCustomer10</div>
+        <div className="promocode-2">bmw-10</div>
       </div>
-      {/* <Box
-        sx={{
-          position: 'absolute',
-          top: '90px',
-          px: '10px',
-          fontSize: 'max(20px, 3vw)',
-          color: 'var(--color2)',
-          borderRadius: '3px',
-          bgcolor: 'var(--background-color1)',
-        }}
-      >
-        Promo codes LuckyCustomer10 
-      </Box> */}
     </div>
   );
 }
